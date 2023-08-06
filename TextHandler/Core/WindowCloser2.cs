a56e0c7c428dd3283d0ae8fd05cacb22ba5c3afe
Артём Лемякин <a.v.lemyakin@gmail.com>
2023-08-06 12:13:29 +0700

    Добавьте файлы проекта.

---
+++ TextHandler/Core/WindowCloser2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextHandler.Core
+{
+    //interface ICloseWindows
+    //{
+    //    Action Close { get; set; }
+    //    bool CanClose();
+    //}
+
+    public class WindowCloser2
+    {
+    //    public static bool GetEnableWindowClosing(DependencyObject obj)
+    //    {
+    //        return (bool)obj.GetValue(EnableWindowClosingProperty);
+    //    }
+
+    //    public static void SetEnableWindowClosing(DependencyObject obj, bool value)
+    //    {
+    //        obj.SetValue(EnableWindowClosingProperty, value);
+    //    }
+
+    //    // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+    //    public static readonly DependencyProperty EnableWindowClosingProperty =
+    //        DependencyProperty.RegisterAttached("EnableWindowClosing", typeof(bool), typeof(WindowCloser), new PropertyMetadata(false, OnEnableWindowClosingChanged));
+
+    //    private static void OnEnableWindowClosingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    //    {
+    //        if (d is Window window)
+    //        {
+    //            window.Loaded += (s, e) =>
+    //            {
+    //                if (window.DataContext is ICloseWindows vm)
+    //                {
+    //                    vm.Close += () =>
+    //                    {
+    //                        window.Close();
+    //                    };
+    //                    window.Closing += (s, e) =>
+    //                    {
+    //                        e.Cancel = !vm.CanClose();
+    //                    };
+    //                }
+    //            };
+    //        }
+    //    }
+    }
+}
---
+++ TextHandler/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using TextHandler.Core;
+using System.Windows;
+using Prism.Commands;
+
+namespace TextHandler.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject, ICloseWindows
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object _currentView;
+
+        private DelegateCommand _closeCommand;
+        public DelegateCommand CloseCommand => _closeCommand ?? (_closeCommand = new DelegateCommand(CloseWindow));
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+            CurrentView = HomeVM;
+            HomeViewCommand = new RelayCommand(o => { CurrentView = HomeVM; });
+            DiscoveryViewCommand = new RelayCommand(o => { CurrentView = DiscoveryVM; });
+        }
+
+        void CloseWindow()
+        {
+            Close?.Invoke();
+        }
+
+        public Action Close { get; set; }
+        public bool CanClose()
+        {
+            return true;
+        }
+    }
+    interface ICloseWindows
+    {
+        Action Close { get; set; }
+        bool CanClose();
+    }
+
+    public class WindowCloser
+    {
+        public static bool GetEnableWindowClosing(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(EnableWindowClosingProperty);
+        }
+
+        public static void SetEnableWindowClosing(DependencyObject obj, bool value)
+        {
+            obj.SetValue(EnableWindowClosingProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EnableWindowClosingProperty =
+            DependencyProperty.RegisterAttached("EnableWindowClosing", typeof(bool), typeof(WindowCloser), new PropertyMetadata(false, OnEnableWindowClosingChanged));
+
+        private static void OnEnableWindowClosingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Window window)
+            {
+                window.Loaded += (s, e) =>
+                {
+                    if (window.DataContext is ICloseWindows vm)
+                    {
+                        vm.Close += () =>
+                        {
+                            window.Close();
+                        };
+                        window.Closing += (s, e) =>
+                        {
+                            e.Cancel = !vm.CanClose();
+                        };
+                    }
+                };
+            }
+        }
+    }
+}
